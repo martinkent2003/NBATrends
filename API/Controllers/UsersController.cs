937bb70ec7ebcf921706872b40cea05886b9dd62
Martin Kent <martinkent@ufl.edu>
2024-03-30 23:03:51 -0400

    connection string not working

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.FromSql($"SELECT * FROM Users").ToList();
+        return users;
+    }
+    [HttpGet("{id}")] // /api/users/3
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = _context.Users.FromSql($"SELECT * FROM Users WHERE Id = {id}").FirstOrDefault();
+        return user;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,17 @@
+using API.Data;
+using Oracle.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseOracle("CiseOracle");
+});
 
 var app = builder.Build();
 
