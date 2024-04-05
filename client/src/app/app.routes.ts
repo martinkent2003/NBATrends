5de6617f102daf33dc99c0852de1958e1298e253
eth-alexander <156004343+eth-alexander@users.noreply.github.com>
2024-04-05 11:13:40 -0400

    made about default page, added form/query page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { VisualizationComponent } from './Pages/visualization/visualization.component';
 import { AboutComponent } from './Pages/about/about.component';
+import { FormComponent } from './Pages/form/form.component';
 
 export const routes: Routes = [
-    { path: '', component: VisualizationComponent },
-    { path: 'about', component: AboutComponent },
+    { path: '', component: AboutComponent },
+    { path: 'visualization', component: VisualizationComponent },
+    { path: 'form', component: FormComponent}
 ];
