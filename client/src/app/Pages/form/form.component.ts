b9da56beaa34e208f16655977f2d01ebb32a3b7d
eth-alexander <156004343+eth-alexander@users.noreply.github.com>
2024-04-05 15:02:53 -0400

    update form page

--- client/src/app/Pages/form/form.component.ts
+++ client/src/app/Pages/form/form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { VisualizationComponent } from '../visualization/visualization.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [VisualizationComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
