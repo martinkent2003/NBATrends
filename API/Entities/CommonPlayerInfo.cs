2c652e438d023f43fbdc0245e2c6bfecf5de5a92
djm18 <93075568+djm18@users.noreply.github.com>
2024-04-06 18:30:23 -0400

    Playerinfo route

--- API/Entities/CommonPlayerInfo.cs
+++ API/Entities/CommonPlayerInfo.cs
@@ -8,20 +8,20 @@ public class CommonPlayerInfo
     [Key]
     [ForeignKey("PlayerId")]
     [NotNull]
-    public int PersonId { get; set; }
+    public int? PersonId { get; set; }
 
     [ForeignKey("TeamId")]
-    public int TeamId{ get; set;}
-    public string Country { get; set; }
-    public string School { get; set; }
-    public int FromYear { get; set; }
-    public int ToYear { get; set; }
-    public string Position { get; set; }
-    public int PlayerWeight { get; set; }
-    public int Height { get; set; }
-    public int JerseyNum { get; set; }
-    public int Seasons { get; set; }
-    public int Greatest75Flag{ get; set;}
-    public int DLeagueFlag{ get; set;}
-    public DateOnly BirthDate{ get; set;}
+    public int? TeamId{ get; set;}
+    public string? Country { get; set; }
+    public string? School { get; set; }
+    public int? FromYear { get; set; }
+    public int? ToYear { get; set; }
+    public string? Position { get; set; }
+    public int? PlayerWeight { get; set; }
+    public int? Height { get; set; }
+    public int? JerseyNum { get; set; }
+    public int? Seasons { get; set; }
+    public int? Greatest75Flag{ get; set;}
+    public int? DLeagueFlag{ get; set;}
+    public DateOnly? BirthDate{ get; set;}
 }
