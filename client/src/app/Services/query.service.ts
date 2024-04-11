60090a86c1ca2551dd6d943d12d4524b612a52f6
Alex Vargas <alex48vargas@gmail.com>
2024-04-10 21:54:47 -0400

    Query service

--- client/src/app/Services/query.service.ts
+++ client/src/app/Services/query.service.ts
@@ -1,8 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { User } from "../Models/user";
 
 @Injectable({providedIn: 'root'})
 export class QueryService {
+  baseUrl = 'http://localhost:5000/api/'
 
   constructor(private http: HttpClient) { }
+
+  getAllUsers() {
+    return this.http.get<User[]>(this.baseUrl + 'users/')
+  }
 }
